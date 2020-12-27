f7fcf1bbf8ebefc031d1878bb364ff4563179465
lawfriends <lawfriends12@gmail.com>
2020-12-27 13:40:46 +0100

    FEAT: added clickable profile drawer header

--- lib/app_drawer.dart
+++ lib/app_drawer.dart
@@ -4,6 +4,47 @@ import 'package:flutter/widgets.dart';
 Drawer a_drawer(_selectedDestination, selectDestination) {
   return Drawer(
     child: ListView(
+      children: <Widget>[
+        InkWell(
+          onTap: () {
+            print("tapped"); // TODO: add navigation to profile page
+          },
+          child: DrawerHeader(
+            margin: EdgeInsets.zero,
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+            child: Stack(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'http://www.bbk.ac.uk/mce/wp-content/uploads/2015/03/8327142885_9b447935ff.jpg'),
+                    radius: 40.0,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    'John Reed',
+                    style: TextStyle(color: Colors.black, fontSize: 20.0),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight + Alignment(0, .3),
+                  child: Text(
+                    'john.reed@mail.com',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        /*child: ListView(
       padding: EdgeInsets.zero,
       children: [
         DrawerHeader(
@@ -28,16 +69,16 @@ Drawer a_drawer(_selectedDestination, selectDestination) {
                         color: Colors.grey,
                         fontSize: 14.0,
                         fontWeight: FontWeight.w500))),
-            Positioned(
+            /* Positioned(
                 bottom: 100.0,
                 left: 16.0,
                 child: ListTile(
                   title: Text('ciao'),
                   selected: _selectedDestination == 2,
                   onTap: () => selectDestination(2),
-                )),
+                )), */
           ]),
-        ),
+        ),*/
         Divider(
           height: 1,
           thickness: 1,
@@ -57,8 +98,8 @@ Drawer a_drawer(_selectedDestination, selectDestination) {
         ListTile(
           leading: Icon(Icons.people),
           title: Text('Friends'),
-          selected: _selectedDestination == 3,
-          onTap: () => selectDestination(3),
+          selected: _selectedDestination == 2,
+          onTap: () => selectDestination(2),
         ),
       ],
     ),

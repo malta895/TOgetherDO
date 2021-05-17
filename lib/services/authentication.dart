94cd8df0bbd1d765b2003df8605304dee7cc82bd
Luca Maltagliati <malta95@hotmail.it>
2021-05-17 22:04:16 +0200

    feat: authenthication class

---
+++ lib/services/authentication.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+class ListAppAuthenticator {
+  ListAppAuthenticator._privateConstructor();
+
+  static final ListAppAuthenticator _instance =
+      ListAppAuthenticator._privateConstructor();
+
+  static ListAppAuthenticator get instance => _instance;
+
+  
+}

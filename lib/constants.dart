e660b76aaba1a290fee539d3b811ca2028a17d51
Luca Maltagliati <malta95@hotmail.it>
2021-07-08 18:00:39 +0200

    fix: changed login page logo

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,5 +2,5 @@ class Constants {
   static const String appName = 'ListApp';
   static const String logoTag = 'com.maltalawrence.listapp';
   static const String titleTag = 'com.maltalawrence.listapp';
-  static const String appLogoPath = 'assets/logo.png';
+  static const String appLogoPath = 'assets/list_default.webp';
 }

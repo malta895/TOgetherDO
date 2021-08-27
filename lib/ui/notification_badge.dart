81e7804f81b20f99a8f33f7bc4047fc9d21e94cb
Luca Maltagliati <malta95@hotmail.it>
2021-08-27 10:09:26 +0200

    fix: center the bell

--- lib/ui/notification_badge.dart
+++ lib/ui/notification_badge.dart
@@ -33,18 +33,21 @@ class _NotificationBadge extends State<NotificationBadge> {
   }
 
   Widget _buildBell() {
-    return Stack(children: <Widget>[
-      Container(height: 56),
-      IconButton(
-        icon: const Icon(Icons.notifications),
-        onPressed: () => {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const NotificationPage()),
-          )
-        },
-      )
-    ]);
+    return Stack(
+      alignment: AlignmentDirectional.center,
+      children: <Widget>[
+        Container(height: 56),
+        IconButton(
+          icon: const Icon(Icons.notifications),
+          onPressed: () => {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const NotificationPage()),
+            )
+          },
+        )
+      ],
+    );
   }
 
   Widget _buildIcon(BuildContext context) {
@@ -64,7 +67,7 @@ class _NotificationBadge extends State<NotificationBadge> {
                       _buildBell(),
                       Positioned(
                         // draw a red marble
-                        top: 7.0,
+                        top: 9.0,
                         right: 7.0,
                         child: Stack(
                           alignment: AlignmentDirectional.center,
